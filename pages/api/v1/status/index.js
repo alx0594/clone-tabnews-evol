4f4360251c01701bea86945eb5dcac3c9f567341
Alex <alexgithub94@gmail.com>
2025-05-03 10:38:51 -0300

    database.js, status/index.js e status/get.test.js concluídos

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,33 @@
-export default function status(request, response) {
+import database from "infra/database.js";
+
+export default async function status(request, response) {
   const update_at = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxconnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseMaxOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseMaxOpenedConnectionsValue =
+    databaseMaxOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     update_at: update_at,
     database: {
       dependencies: {
-        version: "16.8",
-        max_connections: parseInt(100),
-        opened_max_connections: 1,
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxconnectionsValue),
+        opened_max_connections: databaseMaxOpenedConnectionsValue,
       },
     },
   });
